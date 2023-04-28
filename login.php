926f7cf9bb208bfa950ee4b2b1aadae78254bf33
gelgbrl <amg33102@gmail.com>
2023-04-28 23:11:37 +0800

    sprint1 changes
    add and edit form

--- login.php
+++ login.php
@@ -11,51 +11,50 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/crsystem/resource/php/class/core/init.p
   <title>Registrar Portal</title>
   <link rel="stylesheet" type="text/css"  href="vendor/css/bootstrap.min.css">
   <link href="vendor/css/all.css" rel="stylesheet">
-  <link rel="stylesheet" type="text/css"  href="resource/css/styles.css">
+  <link rel="stylesheet" type="text/css"  href="resource/css/login.css">
+  <script src="https://kit.fontawesome.com/9622798f89.js"></script>
 </head>
 <body>
-        <nav class="navbar navbar-dark bg-white shadow-sm slide-in-left">
-          <a class="navbar-brand " href="https://malolos.ceu.edu.ph/">
-            <img src="resource/img/logo.jpg" height="70" class="d-inline-block align-top"
-              alt="mdb logo"><h3 class="ib">
-          </a>
-             <a href="https:/www.facebook.com/theCEUofficial/"><i class="fab fa-facebook-f ceucolor"></i></a>
-             <a href="https://www.instagram.com/ceuofficial/"><i class="fab fa-instagram ceucolor"></i></a>
-             <a href="https://twitter.com/ceumalolos"><i class="fab fa-twitter ceucolor"></i></a>
-        </nav>
-           <div class="container mt-5">
-            <div class="row justify-content-center">
-                <div class="col-8 ">
 
 
-
-                    <form class="text-center border border-light p-5 shadow puff-in-center" action="" method="post" >
-                    <p class="h4 mb-4">Sign in</p>
-                    <?php logd();?>
-                    <input type="text" id="username" class="form-control mb-4" name="username" placeholder="Enter Username" required>
-                    <input type="password" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Enter Password" name ="password" required>
-                    <div class="d-flex justify-content-around">
-                    </div>
-                    <input type =hidden name="token" value="<?php echo Token::generate(); ?>">
-                    <input  type="submit"  class="btn btn-dark btn-block my-4"value="Login"/>
-                    </form>
-
-
-                    <footer id="sticky-footer" class="py-4 bg-dark text-white-50 fixed-bottom slide-in-right">
-                      <div class="container text-center">
-                          <div class="row">
-                              <div class="col col-sm-5 text-left">
-                                  <small>Copyright &copy;Centro Escolar University     Office of the Registrar 2019</small>
-                              </div>
-                              <div class="col text-right">
-                                  <small>Created by: Reymart Bolasoc, Amelia Valencia , James Mangalile, Kenneth De Leon , Pamela Reyes , Ellen Mijares</small>
-                              </div>
-                          </div>
-                      </div>
+             <div class="container-fluid mt-5">
+              <div class="row justify-content-center">
+                <div class="text-center head-count border border-dark">
+                  <h2 class="mb-4 text-center register">Register</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  <input class="btn btn-submit" type="submit" value="Register">
+                  <?php //logd();?>
                 </div>
+
+                <form class="text-center head-count border border-dark p-5 puff-in-center" action="" method="post" >
+                  <h2>Log in </h2>
+                  <?php logd();?>
+                  <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                  <a href="#"><i class="fa-brands fa-google"></i></a>
+                  <h6 class="mb-3">or use your account</h6>
+                  <input type="text" id="username" class="form-control mb-4" name="username" placeholder="Enter Username" required>
+                  <input type="password" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Enter Password" name ="password" required>
+                  <div class="d-flex justify-content-around">
+                  </div>
+                  <h6>Forgot your password?</h6>
+                  <input type =hidden name="token" value="<?php echo Token::generate(); ?>">
+                  <input  type="submit"  class="btn btn-submit my-4"value="Login"/>
+                </form>
+
+                <footer id="sticky-footer" class="py-3 bg-dark text-white-50 fixed-bottom slide-in-right">
+                  <div class="container text-center">
+                    <div class="row">
+                      <div class="col col-sm-5 text-left">
+                        <small>Copyright &copy;Centro Escolar University     Office of the Registrar 2019</small>
+                      </div>
+                      <div class="col text-right">
+                        <small>Created by: Reymart Bolasoc, Amelia Valencia , James Mangalile, Kenneth De Leon , Pamela Reyes , Ellen Mijares</small>
+                      </div>
+                    </div>
+                  </div>
+              </div>
             </div>
-          </div>
-        </footer>
+          </footer>
         <script src="vendor/js/jquery.js"></script>
         <script src="vendor/js/popper.js"></script>
         <script src="vendor/js/bootstrap.min.js"></script>
